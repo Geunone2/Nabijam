e0fcafd99594a08c730b4cfc828287e477113e76
박근원 <circle@baggeun-won-ui-MacBookAir.local>
2024-03-02 18:24:26 +0900

    상담사 리스트 보여주기

---
+++ src/service/Counselors.ts
@@ -0,0 +1,18 @@
+import path from "node:path";
+import {readFile} from "node:fs/promises";
+
+export type Counselor = {
+    name: string;
+    department: string;
+    phoneNumber: string;
+    description: string;
+    path: string;
+}
+
+export async function getAllCounselors(): Promise<Counselor[]> {
+    const filePath = path.join(process.cwd(), 'data', 'counselors.json');
+    return readFile(filePath, 'utf-8')
+        .then<Counselor[]>(JSON.parse)
+        // 날짜 순으로 정렬
+        // .then(counselors => counselors.sort((a, b) => (a.date  > b.date ? -1 : 1)))
+}
