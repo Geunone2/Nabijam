702d1cd76e49c1784e377f6959dc5b53ef2ae176
박근원 <circle@baggeun-won-ui-MacBookAir.local>
2024-03-04 17:44:17 +0900

    모달 기능 추가로 인한 수정사항

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import MultiCarousel from "@/app/components/MultiCarousel";
 import Review from "@/app/components/Review";
 import GiftShopBox from "@/app/components/GiftShopBox";
+import React from "react";
 
 export default function Home() {
     return (
         <>
-            <MultiCarousel/>
             <Review/>
         </>
     );
 }
+
+//<MultiCarousel/> 오류로 잠시 빼놓음
