29712e29a4ed59ab6511d39da84b01921afc8aa2
박근원 <circle@baggeun-won-ui-MacBookAir.local>
2024-03-08 23:09:06 +0900

    로그인, 회원가입 경로 설정

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LoginPage() {
+    return (<div>
+        로그인 페이지
+    </div>);
+}
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RegisterPage() {
+    return (
+        <div>
+            <p>회원가입 페이지</p>
+        </div>
+    );
+}
